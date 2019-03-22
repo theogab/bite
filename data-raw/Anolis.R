60b2b588c78a21f201ad404f72a57d719c4923bc
theogab <theogaboriau@gmail.com>
2019-03-22 17:06:14 +0100

    fix example

--- data-raw/Anolis.R
+++ data-raw/Anolis.R
@@ -14,6 +14,7 @@ for(i in 1:nrow(Anolis_tree$edge)){
   island <- unique(data[data[,1] %in% Anolis_tree$tip.label[descendants(Anolis_tree, Anolis_tree$edge[i,2])],2])
   Anolis_map[i,island == c("cybotes", "sagrei")] <- 1
 }
+Anolis_map <- Anolis_map * Anolis_tree$edge.length
 
 Anolis_traits <- sapply(unique(data[,1]), function(x){
   data$svl.mm[data[,1] == x]
