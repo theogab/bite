15b879adc67ff717fb05e6a1672477bca4d65395
theogab <theogaboriau@gmail.com>
2019-12-06 09:55:51 +0100

    names data

--- data-raw/Anolis.R
+++ data-raw/Anolis.R
@@ -3,16 +3,16 @@ lib <- c("ape", "phytools", "phyloch","jive", "devtools")
 sapply(lib, library, character.only = T)
 
 tree <- read.nexus("data-raw/Anolis_tree.nex")
-tree$tip.label <- sprintf("Anolis_%s", tree$tip.label)
+tree$tip.label <- sprintf("A._%s", tree$tip.label)
 data <- read.table("data-raw/Anolis.txt", header = T, sep = "\t", stringsAsFactors = F)
-data[,1] <- gsub("A.", "Anolis_", data[,1])
+data[,1] <- gsub("A.", "A._", data[,1])
 
 Anolis_tree <- drop.tip(tree, tree$tip.label[!tree$tip.label %in% unique(data[,1])])
 
 Anolis_map <- matrix(0, nrow = nrow(Anolis_tree$edge), ncol = 2, dimnames = list(apply(Anolis_tree$edge, 1, paste0, collapse = ","), c("H", "C")))
 for(i in 1:nrow(Anolis_tree$edge)){
   island <- unique(data[data[,1] %in% Anolis_tree$tip.label[descendants(Anolis_tree, Anolis_tree$edge[i,2])],2])
-  Anolis_map[i,island == c("cybotes", "sagrei")] <- 1
+  Anolis_map[i,island == c("Hispaniola", "Cuba")] <- 1
 }
 Anolis_map <- Anolis_map * Anolis_tree$edge.length
 
